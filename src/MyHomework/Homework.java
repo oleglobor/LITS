04747b723d7c993d3d23d20966e6f03b8299201f
oleglobor <olegromanovichlviv@gmail.com>
2021-07-14 18:49:27 +0300

    homework

---
+++ src/MyHomework/Homework.java
@@ -0,0 +1,9 @@
+package MyHomework;
+
+public class Homework {
+    public static void main(String[] args) {
+        System.out.println("\u0014");
+        System.out.println("\u0077");
+        System.out.println("\u0105");
+    }
+}
