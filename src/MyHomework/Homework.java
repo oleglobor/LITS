2546b7c81249e653d109fc08d6751852f60c423f
oleglobor <olegromanovichlviv@gmail.com>
2021-07-14 21:27:34 +0300

    homework

--- src/MyHomework/Homework.java
+++ src/MyHomework/Homework.java
@@ -2,13 +2,34 @@ package MyHomework;
 
 public class Homework {
     public static void main(String[] args) {
-        int c = 14;
-        int d = 77;
-        int w = 105;
+        int a = 23;
+        int b= 5;
+        int e = 8;
+        int c = 2;
+        int i = 3;
+        int z = 19;
+        int x = 14;
+        int p = 7;
+        int o = 40;
 
-        System.out.println((char)c);
-        System.out.println((char)d);
-        System.out.println((char)w);
+        int I= 2;
+
+        int result1 = a+b/e+c*(i+a);
+        int result2 = z+x-e+p/a;
+        int result3 = o+i+I*(a+b);
+        System.out.println("a + b / e + c * (i + a) = " + result1);
+        System.out.println("z + x - e + p / a = " + result2);
+        System.out.println("o + i + I * (a + b) = " + result3);
+
+        System.out.println((char)14);
+        System.out.println((char)77);
+        System.out.println((char)105);
+
+        System.out.println("Number 10 is even - " + (10%2 ==0));
+        System.out.println("Number 13 is even - " + (13%2 ==0));
+        System.out.println("Number 17 is even - " + (17%2 ==0));
+        System.out.println("Number 20 is even - " + (20%2 ==0));
+
 
     }
 }
